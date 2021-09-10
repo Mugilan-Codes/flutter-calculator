2abc1a23d3c5f845da2270944247bb2e0b722765
Mugilan <mugilancodes@gmail.com>
2021-09-10 14:57:37 +0530

    basic functionality

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,6 +8,50 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var num1 = 0, num2 = 0, result = 0;
+
+  TextEditingController t1 = TextEditingController(text: "0");
+  TextEditingController t2 = TextEditingController(text: "0");
+
+  void doAddition() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      result = num1 + num2;
+    });
+  }
+
+  void doSubtraction() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      result = num1 - num2;
+    });
+  }
+
+  void doMultiplication() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      result = num1 * num2;
+    });
+  }
+
+  void doDivision() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      result = num1 ~/ num2;
+    });
+  }
+
+  void doClear() {
+    setState(() {
+      t1.text = "";
+      t2.text = "";
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,40 +61,42 @@ class _HomePageState extends State<HomePage> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Text(
-                  "Output: ",
-                  style: TextStyle(
+                Text(
+                  "Output: $result",
+                  style: const TextStyle(
                       fontSize: 20.0,
                       fontWeight: FontWeight.bold,
                       color: Colors.purple),
                 ),
-                const TextField(
+                TextField(
                   keyboardType: TextInputType.number,
-                  decoration: InputDecoration(hintText: "Enter number 1"),
+                  decoration: const InputDecoration(hintText: "Enter number 1"),
+                  controller: t1,
                 ),
-                const TextField(
+                TextField(
                   keyboardType: TextInputType.number,
-                  decoration: InputDecoration(hintText: "Enter number 2"),
+                  decoration: const InputDecoration(hintText: "Enter number 2"),
+                  controller: t2,
                 ),
                 const Padding(padding: EdgeInsets.only(top: 20.0)),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     ElevatedButton(
-                      onPressed: () {},
+                      onPressed: () => doAddition(),
                       child: const Text("+"),
                       style: ElevatedButton.styleFrom(primary: Colors.green),
                     ),
                     ElevatedButton(
-                        onPressed: () {},
+                        onPressed: () => doSubtraction(),
                         child: const Text("-"),
                         style: ElevatedButton.styleFrom(primary: Colors.green)),
                     ElevatedButton(
-                        onPressed: () {},
+                        onPressed: () => doMultiplication(),
                         child: const Text("*"),
                         style: ElevatedButton.styleFrom(primary: Colors.green)),
                     ElevatedButton(
-                        onPressed: () {},
+                        onPressed: () => doDivision(),
                         child: const Text("/"),
                         style: ElevatedButton.styleFrom(primary: Colors.green))
                   ],
